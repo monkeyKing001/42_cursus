9e2494562e3fbf3e2e58aa4007a599a46cca3dfc
Dong woo Kwak <dokwak@c9r5s5.42seoul.kr>
2021-12-02 21:58:15 +0900

    part 2

--- libft/srcs/ft_split.c
+++ libft/srcs/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dokwak <dokwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 13:46:55 by dokwak            #+#    #+#             */
-/*   Updated: 2021/12/02 17:55:51 by dokwak           ###   ########.fr       */
+/*   Updated: 2021/12/02 21:09:15 by dokwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,11 +17,13 @@ long long	count_chnk_num(char const *s, char c)
 
 	s_pnt1 = s;
 	chnk_num = 0;
-	while (s)
+	while (*s)
 	{
 		s_pnt1 = ft_strchr(s, c);
 		if (s_pnt1 != s)
 			chnk_num++;
+		if (!s_pnt1)
+			break ;
 		s = s_pnt1 + 1;
 	}
 	return (chnk_num);
@@ -29,26 +31,25 @@ long long	count_chnk_num(char const *s, char c)
 
 char	**ft_split(char const *s, char c)
 {
-	long long	chnk_num;
 	long long	i;
 	char const	*s_pnt1;
 	char		**ret_pnt;
 
-	chnk_num = count_chnk_num(s, c);
-	ret_pnt = malloc(sizeof(char *) * (chnk_num + 1));
-	if (chnk_num == 0 || ret_pnt == 0)
+	ret_pnt = malloc(sizeof(char *) * (count_chnk_num(s, c) + 1));
+	if (ret_pnt == NULL)
 		return (NULL);
 	s_pnt1 = s;
 	i = 0;
-	while (s)
+	while (*s)
 	{
 		s_pnt1 = ft_strchr(s, c);
-		if (s_pnt1 != s)
+		if (s_pnt1 == NULL)
 		{
-			if (s_pnt1)
-				s_pnt1 = 0;
 			ret_pnt[i++] = ft_substr(s, 0, ft_strlen((char *)s));
+			break ;
 		}
+		if (s_pnt1 != s)
+			ret_pnt[i++] = ft_substr(s, 0, s_pnt1 - s);
 		s = s_pnt1 + 1;
 	}
 	ret_pnt[i] = NULL;
